bca94ffe33fb70c620adbddfac0897b4ca5ac60f
Steven <spysschaert.steven0@gmail.com>
2019-09-29 21:46:46 +0200

    Event Listener added and empty functions nextSlide, previousSlide, nextSlideArrow added too

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,3 +3,14 @@
 const nextButton = document.querySelector(".next");
 const previousButton = document.querySelector(".previous");
 const li = document.querySelectorAll("li");
+let i = 0;
+
+function nextSlide() {}
+
+function previousSlide() {}
+
+function nextSlideArrow() {}
+
+nextButton.addEventListener("click", nextSlide);
+previousButton.addEventListener("click", previousSlide);
+document.addEventListener("keyup", nextSlideArrow);
